26ac481c86084e02ab8b013493b3d79d3e706fb3
xenwithu <lvd.levandat@gmail.com>
2024-10-15 16:32:02 +0700

    fix: add babel

---
+++ src/pages/PortfolioPage/components/CardHorizontal/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import BrandLogo from '../BrandLogo';
+interface CardHorizontalProps {
+    children?: React.ReactNode;
+}
+
+const CardHorizontalItem: React.FC<CardHorizontalProps> = (props) => (
+    <></>
+);
+
+export default CardHorizontalItem;
---
+++ src/pages/PortfolioPage/components/SectionBreak/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+interface HeaderProps {
+  
+}
+
+const SectionBreak: React.FC<HeaderProps> = () => (
+    <div className={`py-3 px-10 flex items-center justify-between max-md:justify-center`}>
+   
+    </div>
+);
+
+export default SectionBreak;
